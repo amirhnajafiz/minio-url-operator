a6f96dfabc6a67149c464c8d34c91077d35d59e8
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-07 23:33:57 +0430

    host config init

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
  */
 #include <stdio.h> /* printf, sprintf */
 #include <stdlib.h> /* exit */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
 
 // error message handler
 void error(const char *msg) {
@@ -18,6 +20,18 @@ void error(const char *msg) {
     exit(-1);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    // defining the configs of our domain
+    int port = 80;
+    char *host = "https://www.google.com";
+    char *message = "";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+
+    if (argc < 3) {
+        error("Parameters required: <domain> <command>");
+    }
+
     return 0;
 }
