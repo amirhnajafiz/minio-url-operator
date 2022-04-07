39c36a7b14c3313b4f26708a033be7e0603b86b6
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-07 23:23:48 +0430

    initialize project

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * C-ping
+ * author: amirhnajafiz
+ * year: 2022
+ * email: najafizadeh21@gmail.com
+ *
+ * description:
+ * c-ping is a service to check our http services by sending
+ * requests to our domains and then processing the response.
+ *
+ */
+
+int main() {
+    return 0;
+}
