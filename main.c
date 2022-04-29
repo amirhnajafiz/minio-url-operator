6332b0aef2844251b7ce9a64d0c0e6b1f486bdbb
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-29 22:51:24 +0430

    ping loop added

--- main.c
+++ main.c
@@ -9,11 +9,23 @@
  * requests to our domains and then processing the response.
  *
  */
+#include "stdbool.h"
+
 #include "error/error.c"
 #include "api/api.c"
 
 int main(int argc, char *argv[]) {
-    execute(argc, argv);
+
+    while (true) {
+        int res = execute(argc, argv);
+        if (res != 1) {
+            error("![Panic] Ping failed");
+
+            break;
+        }
+
+        sleep(2000);
+    }
 
     return 0;
 }
