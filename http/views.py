957b9bc95348e526ed558dc8c2579dfbe418f086
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-14 10:42:34 +0330

    update: views api

--- http/views.py
+++ http/views.py
@@ -6,9 +6,19 @@ views_blueprint = Blueprint('views_blueprint', __name__)
 
 
 @views_blueprint.route('/', methods=['GET'])
-def index():
+def index_page():
     """home page of application
 
     :return: index.html template
     """
     return render_template('index.html')
+
+
+@views_blueprint.route('/docs', methods=['GET'])
+def help_page():
+    return render_template('help.html')
+
+
+@views_blueprint.route('/docs/snappline', methods=['GET'])
+def snappline_help_page():
+    return render_template('help.snappline.html')
