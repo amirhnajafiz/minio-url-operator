40a3088b578f3419b51b4403389ad1c2f0b76884
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-14 09:42:52 +0330

    init: url api

--- http/api.py
+++ http/api.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 
 # create a blueprint for application apis
 api_blueprint = Blueprint('api_blueprint', __name__, url_prefix="/api")
+
+
+@api_blueprint.route("/url", methods=['GET'])
+def get_urls():
+    pass
