fd2e69c2e1e323ae92d3f3bb8c1a55b927408404
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-14 10:47:06 +0330

    add: handler of http

--- http/api.py
+++ http/api.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
+from .handler import Handler
 
 
 # create a blueprint for application apis
 api_blueprint = Blueprint('api_blueprint', __name__, url_prefix="/api")
 
+# create a new handler
+api = Handler()
 
-@api_blueprint.route("/url", methods=['GET'])
-def get_urls():
-    pass
+
+@api_blueprint.route("/objects", methods=['GET'])
+def get_objects():
+    api.get_objects()
---
+++ http/handler.py
@@ -0,0 +1,7 @@
+class Handler(object):
+    def __init__(self):
+        self.database = None
+        self.minio = None
+
+    def get_objects(self):
+        pass
