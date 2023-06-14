f0a774f4b589958c195cb5329fa6580bbdd3b4fc
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-14 09:30:32 +0330

    add: url model

--- model/url.py
+++ model/url.py
@@ -1,3 +1,34 @@
+import sqlite3
+from datetime import datetime
+
+
 class URL(object):
-    def __int__(self):
-        pass
+    """URL is our url database model"""
+
+    def __init__(self, key, url):
+        self.id = None
+        self.key = key
+        self.url = url
+        self.createdAt = datetime.now()
+
+    def read(self, row: sqlite3.Row):
+        """read values from database row
+
+        :param row: database row
+        :return: None
+        """
+        self.id = row[0]
+        self.key = row[1]
+        self.url = row[2]
+        self.createdAt = row[3]
+
+    def write(self) -> list:
+        """write values into a list
+
+        :return: list
+        """
+        return [
+            self.key,
+            self.url,
+            self.createdAt
+        ]
