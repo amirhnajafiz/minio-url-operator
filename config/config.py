6eded053c61768a4e24f9e48d2e8c221698544a9
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-15 17:07:44 +0330

    update: configs

--- config/config.py
+++ config/config.py
@@ -8,7 +8,16 @@ class Config(object):
         self.port = None
         self.debug = True
 
+        self.minio = {}
+        self.sql = {}
+
     def load(self):
         """"load configs into class fields"""
         self.port = int(read_value_from_env("HTTP_PORT"))
         self.debug = bool(read_value_from_env("HTTP_DEBUG"))
+
+        self.minio['host'] = read_value_from_env("MINIO_HOST")
+        self.minio['access'] = read_value_from_env("MINIO_ACCESS")
+        self.minio['secret'] = read_value_from_env("MINIO_SECRET")
+
+        self.sql['host'] = read_value_from_env("SQL_HOST")
