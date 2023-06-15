69745ad4e4574a9e549475b46b82fdca762c25f1
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-15 10:08:47 +0330

    update: views class

--- http/views.py
+++ http/views.py
@@ -1,24 +1,26 @@
 from flask import Blueprint, render_template
 
 
-# create a blueprint for views
-views_blueprint = Blueprint('views_blueprint', __name__)
+class Views(object):
+    def __init__(self):
+        # create a blueprint for views
+        self.blueprint = Blueprint('views_blueprint', __name__)
 
+        @self.blueprint.route('/', methods=['GET'])
+        def index_page():
+            """home page of application
 
-@views_blueprint.route('/', methods=['GET'])
-def index_page():
-    """home page of application
+            :return: index.html template
+            """
+            return render_template('index.html')
 
-    :return: index.html template
-    """
-    return render_template('index.html')
+        @self.blueprint.route('/docs', methods=['GET'])
+        def help_page():
+            return render_template('help.html')
 
+        @self.blueprint.route('/docs/snappline', methods=['GET'])
+        def snappline_help_page():
+            return render_template('help.snappline.html')
 
-@views_blueprint.route('/docs', methods=['GET'])
-def help_page():
-    return render_template('help.html')
-
-
-@views_blueprint.route('/docs/snappline', methods=['GET'])
-def snappline_help_page():
-    return render_template('help.snappline.html')
+    def get_blue_print(self):
+        return self.blueprint
