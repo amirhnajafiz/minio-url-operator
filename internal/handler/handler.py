a15ad957ac193eee724af520d6b8b317f1f46b95
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-17 21:46:24 +0330

    add: get object url handler

--- internal/handler/handler.py
+++ internal/handler/handler.py
@@ -9,8 +9,17 @@ class Handler(object):
         self.database = database
         self.minio_connection = minio_connection
 
-    def get_objects(self):
+    def get_objects(self, bucket, prefix=""):
         pass
 
-    def get_object_url(self):
-        pass
+    def get_object_url(self, bucket, key):
+        # get a new cursor
+        cursor = self.database.get_cursor()
+
+        # select a url from database
+        cursor.execute(f'SELECT * FROM object_urls WHERE bucket = ? AND object = ?', [bucket, key])
+
+        # todo: [1] if not exists create one
+        # todo: [2] if exists check the url time past 7 days
+        # todo: [3] if 1 or 2 create a new link
+        # todo: [4] return the link
