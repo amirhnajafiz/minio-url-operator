fc71fe452e05dcb92efa9d5616c5885e617dd4c9
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-12 10:41:20 +0330

    fix: migrations scripts

--- database/migrations/up-migration-001-urls_table.sql
+++ database/migrations/up-migration-001-urls_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE urls (
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    bucket     VARCHAR(1024) NOT NULL,
-    key        VARCHAR(1024) NOT NULL,
-    url        VARCHAR(2048) NOT NULL,
-    created_at DATETIME
+    id                INTEGER PRIMARY KEY AUTO_INCREMENT,
+    bucket            VARCHAR(1024) NOT NULL,
+    object_key        VARCHAR(1024) NOT NULL,
+    url               VARCHAR(2048) NOT NULL,
+    created_at        DATETIME
 );
